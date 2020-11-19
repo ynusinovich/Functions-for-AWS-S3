574a4e3fcb63b45563b714ed9d82890b1dfc0368
Yan A. Nusinovich <yan.nusinovich@gmail.com>
2020-11-18 23:00:41 -0500

    fix: adding clearer attributions to README
    Co-authored-by: Paul Brit <paulbrit44@gmail.com>

--- functions-for-csvs-and-models-aws-s3.py
+++ functions-for-csvs-and-models-aws-s3.py
@@ -10,13 +10,17 @@ client = boto3.client('s3',
                       aws_secret_access_key = '**************************')
 bucket_name = '***********' # name of the main folder (bucket) in S3
 
-
-def upload_csv_to_s3(folder_path, filename, dataframe, upload_index):
+    
+def multipart_upload_csv_to_s3(folder_path, filename, dataframe, upload_index):
+    # Set the desired multipart threshold value (5 GB)
+    GB = 1024 ** 3
+    config = TransferConfig(multipart_threshold = 5 * GB)
     # upload_index should be True if the index should be saved or False if it shouldn't
     object_key = folder_path + filename
     csv_buffer = StringIO()
     dataframe.to_csv(csv_buffer, index = upload_index)
-    client.put_object(Bucket = bucket_name, Key = object_key, Body = csv_buffer.getvalue())
+    csv_buffer_2 = BytesIO(csv_buffer.getvalue().encode())
+    client.upload_fileobj(csv_buffer_2, Bucket = bucket_name, Key = object_key, Config = config)
     
 def load_csv_from_s3(folder_path, filename, upload_index):
     # upload_index should be equal to the number of the column to use as the index column, or False if there is no index column
@@ -27,10 +31,13 @@ def load_csv_from_s3(folder_path, filename, upload_index):
     df = pd.read_csv(StringIO(csv_string), index_col = upload_index)
     return df
 
-def upload_model_to_s3(folder_path, filename, model):
+def multipart_upload_model_to_s3(folder_path, filename, model):  
+    # Set the desired multipart threshold value (5 GB)
+    GB = 1024 ** 3
+    config = TransferConfig(multipart_threshold = 5 * GB)
     object_key = folder_path + filename
     fo = BytesIO(pickle.dumps(model)) # adjust if you will not be using pickle to save your model
-    client.upload_fileobj(fo, Bucket = bucket_name, Key = object_key)
+    client.upload_fileobj(fo, Bucket = bucket_name, Key = object_key, Config = config)
     
 def load_model_from_s3(folder_path, filename):
     object_key = folder_path + filename
